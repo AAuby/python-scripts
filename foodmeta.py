e495d77f995d1b406ff1b946b34ee353e9d8bd49
gongneilianhua2 <1665353286@qq.com>
2021-03-26 17:20:21 +0800

    Update foodmeta.py

--- foodmeta.py
+++ foodmeta.py
@@ -3,7 +3,6 @@ from lxml import etree
 
 
 base_url = "http://db.foodmate.net/yingyang/"
-food_meta = []
 
 category = open("category.txt", "w+", encoding="UTF-8")
 
@@ -23,11 +22,7 @@ for element in category_resp_parse.xpath('//div[@id="main2"]//a'):
         food_resp = requests.get(f'{base_url}{link}')
         food_resp_parse = etree.HTML(food_resp.text)
         category.writelines(content+"\n")
-        food = {
-            "food_name": content,
-            "detail": {}
-        }
-        for f in food_resp_parse.xpath('//div[@class="list"]'):
-            food["detail"][f.getchildren()[0].text] = f.xpath('text()')[0] if f.xpath('text()') else 0
-        food_meta.append(food)
+        for fd in food_resp_parse.xpath('//div[@class="list"]'):
+            food["detail"][fd.getchildren()[0].text] = fd.xpath('text()')[0] if fd.xpath('text()') else 0
+            category.write(f"\t{fd.getchildren()[0].text} -> {fd.xpath('text()')}\n")
 category.close()
